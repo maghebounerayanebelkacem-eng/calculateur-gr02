d91b4232a003400452abda4993863e01b33832b0
maghebounerayanebelkacem-eng <maghebounerayanebelkacem@gmail.com>
2026-05-27 15:01:31 +0100

    Update calculateur.py

--- calculateur.py
+++ calculateur.py
@@ -1,6 +1,7 @@
 def calculer_tva(prix_ht :float) -> float:
     return prix_ht *1.20
-
+def convertir_euros_dollars(montant_euro:float) -> float:
+    return montant_euro *1.08
 
 prix_ttc = calculer_tva(100)
 print(f"PrixTTC: {prix_ttc}")
